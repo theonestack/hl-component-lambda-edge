e0fbe4b0fb56f2a3beba4a85954a89cbc2632c90
Aaron Walker <a.walker@base2services.com>
2022-05-05 17:06:41 +0000

    simple inlined edge function

--- lambda-edge.cfhighlander.rb
+++ lambda-edge.cfhighlander.rb
@@ -1,5 +1,8 @@
 CfhighlanderTemplate do
   
+  DependsOn 'lib-iam@0.2.0'
+  DependsOn 'lib-ec2@0.2.1'
+  
   Parameters do
     ComponentParam 'EnvironmentName', 'dev', isGlobal: true
     ComponentParam 'EnvironmentType', 'development', allowedValues: ['development','production'], isGlobal: true
--- lambda-edge.cfndsl.rb
+++ lambda-edge.cfndsl.rb
@@ -1,7 +1,54 @@
 CloudFormation do
 
-    #Remove me
-    SNS_Topic(:Example) do
+  tags = []
+  tags << { Key: 'Environment', Value: Ref(:EnvironmentName) }
+  tags << { Key: 'EnvironmentType', Value: Ref(:EnvironmentType) }
+  
+  extra_tags.each { |key,value| tags << { Key: key, Value: value } } if defined? extra_tags
+
+  functions =  external_parameters.fetch(:functions, {})
+  
+
+  functions.each do |function_name, lambda_config|
+    policies = []
+    lambda_config['policies'].each do |name,policy|
+      policies << iam_policy_allow(name,policy['action'],policy['resource'] || '*')
+    end if lambda_config.has_key?('policies')
+
+    IAM_Role("#{function_name}Role") do
+      AssumeRolePolicyDocument service_role_assume_policy(['edgelambda', 'lambda'])
+      Path '/'
+      Policies policies if policies.any?
+      ManagedPolicyArns external_parameters['managed_policies']
     end
+
+    environment = lambda_config['environment'] || nil
+    # Create Lambda function
+    Lambda_Function(function_name) do
+      Code do
+        ZipFile(FnSub(lambda_config['code']))
+      end
+      Environment(Variables: Hash[environment.collect { |k, v| [k, v] }]) unless environment.nil?
+      Handler(lambda_config['handler'] || 'index.handler')
+      MemorySize(lambda_config['memory'] || 128)
+      Role(FnGetAtt("#{function_name}Role", 'Arn'))
+      Runtime(lambda_config['runtime'])
+      Timeout(lambda_config['timeout'] || 10)
+      if !lambda_config['named'].nil? && lambda_config['named']
+        FunctionName(lambda_config['function_name'] || function_name)
+      end
+      Tags tags
+    end
+
+    Lambda_Version("#{function_name}Version") do
+      FunctionName Ref(function_name)
+    end
+
+    Output("#{function_name}Version") do
+      Value Ref(function_name)
+      Export FnSub("${EnvironmentName}-#{external_parameters[:component_name]}-#{function_name}Version")
+    end
+
+  end
 
 end
